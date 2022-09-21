8002621ea6988df1c729398d8d3a36d0d224f2c5
Hasin Mahmood <hasinmahmood01@gmail.com>
2022-09-21 17:07:35 +0100

    added comments to js

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
+//query selector for the grid container (the grids parent container)
 const gridContainer = document.querySelector(".grid-container");
 
+//variable that stores the width of the grid container
 const gridWidth = gridContainer.offsetWidth;
-console.log(gridWidth);
 
-const createGrid = (width) => {
+//function used to create grid
+const createGrid = (numberOfCells) => {
   const col = document.createElement("div");
   col.className = "col";
 
-  for (let y = 0; y < width; y++) {
+  for (let y = 0; y < numberOfCells; y++) {
     const row = document.createElement("div");
     row.className = "row";
-    for (let x = 0; x < width; x++) {
+    for (let x = 0; x < numberOfCells; x++) {
       const cell = document.createElement("div");
-      cell.style.cssText = `width:${gridWidth / width}px; height:${
-        gridWidth / width
+      cell.style.cssText = `width:${gridWidth / numberOfCells}px; height:${
+        gridWidth / numberOfCells
       }px; border: 0.1px solid black;`;
       row.appendChild(cell);
     }
