b9cacf940380cc69f24d1c2160f5bef52cac37d5
Hasin Mahmood <hasinmahmood01@gmail.com>
2022-09-21 13:44:15 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const gridContainer = document.querySelector(".grid-container");
+// gridContainer.style.cssText = "background-color: pink; height: 600px";
+
+const createGrid = (width) => {
+  for (let i = 0; i < width; i++) {
+    console.log(i);
+  }
+};
+
+createGrid(10);
