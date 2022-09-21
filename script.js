304619196893416cdfb999389696ed1a8fc44233
Hasin Mahmood <hasinmahmood01@gmail.com>
2022-09-21 16:48:58 +0100

    determine grid cell size dynamically based on container width and how many cells

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 const gridContainer = document.querySelector(".grid-container");
-// gridContainer.style.cssText = "background-color: pink; height: 600px";
+
+const gridWidth = gridContainer.offsetWidth;
+console.log(gridWidth);
 
 const createGrid = (width) => {
   const col = document.createElement("div");
-  col.className = "display:flex; flex-direction: col;";
+  col.className = "col";
+
   for (let y = 0; y < width; y++) {
     const row = document.createElement("div");
-    row.style.cssText = "display:flex;";
+    row.className = "row";
     for (let x = 0; x < width; x++) {
       const cell = document.createElement("div");
-      cell.style.cssText = "width:25px; height:25px; border: 1px solid black;";
+      cell.style.cssText = `width:${gridWidth / width}px; height:${
+        gridWidth / width
+      }px; border: 0.1px solid black;`;
       row.appendChild(cell);
     }
     col.appendChild(row);
@@ -17,4 +22,4 @@ const createGrid = (width) => {
   gridContainer.appendChild(col);
 };
 
-createGrid(16);
+createGrid(20);
