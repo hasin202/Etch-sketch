85f3cb7caeb65c7dfb8b941684974f739905dfdb
Hasin Mahmood <hasinmahmood01@gmail.com>
2022-09-21 15:57:21 +0100

    Revert "]]]]"
    This reverts commit 2b4ea4bfbbff43f74feceb12573a89f3ad263cae.

--- script.js
+++ script.js
@@ -1,20 +1,15 @@
 const gridContainer = document.querySelector(".grid-container");
-
-const gridWidth = gridContainer.offsetWidth;
-console.log(gridWidth);
+// gridContainer.style.cssText = "background-color: pink; height: 600px";
 
 const createGrid = (width) => {
   const col = document.createElement("div");
-  col.className = "col";
-
+  col.className = "display:flex; flex-direction: col;";
   for (let y = 0; y < width; y++) {
     const row = document.createElement("div");
-    row.className = "row";
+    row.style.cssText = "display:flex;";
     for (let x = 0; x < width; x++) {
       const cell = document.createElement("div");
-      cell.style.cssText = `width:${gridWidth / width}px; height:${
-        gridWidth / width
-      }px; border: 0.1px solid black;`;
+      cell.style.cssText = "width:25px; height:25px; border: 1px solid black;";
       row.appendChild(cell);
     }
     col.appendChild(row);
