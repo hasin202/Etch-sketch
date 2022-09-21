54270de416c907c19e68329f469552e39e569e88
Hasin Mahmood <hasinmahmood01@gmail.com>
2022-09-21 14:11:06 +0100

    render full gird using js

--- script.js
+++ script.js
@@ -2,11 +2,19 @@ const gridContainer = document.querySelector(".grid-container");
 // gridContainer.style.cssText = "background-color: pink; height: 600px";
 
 const createGrid = (width) => {
-  for (let i = 0; i < width; i++) {
-    const col = document.createElement("div");
-    col.style.cssText = "width:40px; height:40px; background-color: blue;";
-    gridContainer.appendChild(col);
+  const col = document.createElement("div");
+  col.className = "display:flex; flex-direction: col;";
+  for (let y = 0; y < width; y++) {
+    const row = document.createElement("div");
+    row.style.cssText = "display:flex;";
+    for (let x = 0; x < width; x++) {
+      const cell = document.createElement("div");
+      cell.style.cssText = "width:25px; height:25px; border: 1px solid black;";
+      row.appendChild(cell);
+    }
+    col.appendChild(row);
   }
+  gridContainer.appendChild(col);
 };
 
-createGrid(2);
+createGrid(16);
