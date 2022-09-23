8e40da734cd31e001460bb3f733d8e73b82c2e12
Hasin Mahmood <hasinmahmood01@gmail.com>
2022-09-23 21:33:49 +0100

    fixed mouse events from firing too many times

--- script.js
+++ script.js
@@ -1,33 +1,19 @@
-//query selector for the grid container (the grids parent container)
 const gridContainer = document.querySelector(".grid-container");
-
-let cellColor = "black";
-
-//an object to store hex colors and their names
-const colors = {
-  White: "#FFFFFF",
-  Black: "#000000",
-  Red: "#FF2D00",
-  Blue: "#002BFF",
-  LightSalmon: "#FFA07A",
-  IndianRed: "#CD5C5C",
-  DarkRed: "#8B0000",
-  LightGreen: "#90EE90	",
-  MediumSpringGreen: "#00FA9A	",
-  MediumSeaGreen: "#3CB371",
-  paleTurquoise: "#40E0D0",
-  aqua: "#00FFFF",
-  SteelBlue: "#4682B4",
-};
-
 const gridWidth = gridContainer.offsetWidth;
 
-//function to return a nodelist of all of the nodes that have an id of cell
-const selectCell = () => {
-  return (cellQuerySelectAll = document.querySelectorAll("#cells"));
+let cellColor = "black";
+let mouseDown = false;
+
+const setGridContainerEventListener = () => {
+  gridContainer.addEventListener("mousedown", () => {
+    mouseDown = true;
+  });
+  gridContainer.addEventListener("mouseup", () => {
+    console.log("down");
+    mouseDown = false;
+  });
 };
 
-//function to wipe grid
 const wipeGrid = () => {
   gridContainer.innerHTML = "";
 };
@@ -49,19 +35,13 @@ const createGrid = (numberOfCells) => {
         gridWidth / numberOfCells
       }px;`;
       cell.id = "cells";
-      let mouseDown = false;
-      gridContainer.addEventListener("mousedown", () => {
-        mouseDown = true;
-        cell.addEventListener("mousemove", () => {
-          if (mouseDown) {
-            colorCell(cell, cellColor);
-          }
-        });
+
+      cell.addEventListener("mousemove", () => {
+        if (mouseDown) {
+          colorCell(cell, cellColor);
+        }
       });
-      gridContainer.addEventListener("mouseup", () => {
-        console.log("up");
-        mouseDown = false;
-      });
+
       row.appendChild(cell);
     }
     col.appendChild(row);
@@ -70,7 +50,6 @@ const createGrid = (numberOfCells) => {
 };
 
 function ready(callback) {
-  // in case the document is already rendered
   if (document.readyState != "loading") callback();
   else if (document.addEventListener)
     document.addEventListener("DOMContentLoaded", callback);
@@ -80,10 +59,10 @@ function ready(callback) {
     });
 }
 
-//function only runs once
 ready(function () {
   const slider = document.getElementById("slider");
 
+  setGridContainerEventListener();
   createGrid(1);
 
   slider.addEventListener("input", () => {
